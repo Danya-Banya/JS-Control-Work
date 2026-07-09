af82036f21c04966b9c89fa0af7f8cf26a8406fd
Danya-Banya <daniyarmurataliev86@gmail.com>
2026-07-09 20:23:24 +0600

    Контрольная работа scss

---
+++ main.js
@@ -0,0 +1,6 @@
+function showMenu(){
+    document.querySelector('.header__item--mediaSmall').classList.remove('hidden')
+}
+function hideMenu(){
+    document.querySelector('.header__item--mediaSmall').classList.add('hidden')
+}
